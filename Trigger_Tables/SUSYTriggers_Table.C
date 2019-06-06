0c3a178c8bd7bd3fe33626b190afebdc8b574bbb
Matt Zhang <set.stun@gmail.com>
2019-06-06 19:25:44 +0200

    Corrected and updated trigger tables

--- Trigger_Tables/SUSYTriggers_Table.C
+++ Trigger_Tables/SUSYTriggers_Table.C
@@ -5,14 +5,13 @@ using namespace std;
 
 void SUSYTriggers_Table() {
 
-    vector<string> file_names {"SUSY2_Data/data15-16", "SUSY2_Data/data17", "SUSY2_Data/data18", "SUSY2_Signal_mc16a/C1N2_WZ_600_0_2L2J", "SUSY2_Signal_mc16cd/C1N2_WZ_600_0_2L2J", "SUSY2_Signal_mc16e/C1N2_WZ_600_0_2L2J", "SUSY2_Signal_mc16a/C1N2_WZ_200p0_100p0_2L2J", "SUSY2_Signal_mc16a/C1N2_WZ_200p0_100p0_2L2J", "SUSY2_Signal_mc16a/C1N2_WZ_200p0_100p0_2L2J"};
+    vector<string> file_names {"SUSY2_Data/data15-16", "SUSY2_Data/data17", "SUSY2_Data/data18", "SUSY2_Signal_mc16a/C1N2_WZ_600_0_2L2J", "SUSY2_Signal_mc16cd/C1N2_WZ_600_0_2L2J", "SUSY2_Signal_mc16e/C1N2_WZ_600_0_2L2J", "SUSY2_Signal_mc16a/C1N2_WZ_200p0_100p0_2L2J", "SUSY2_Signal_mc16cd/C1N2_WZ_200p0_100p0_2L2J", "SUSY2_Signal_mc16e/C1N2_WZ_200p0_100p0_2L2J"};
     vector<string> file_labels {"data15-16", "data17", "data18", "(600, 0) MC16a", "(600, 0) MC16cd", "(600, 0) MC16e", "(200, 100) MC16a", "(200, 100) MC16cd", "(200, 100) MC16e"};
-    vector<string> triggers {"trigMatch_1L2LTrig", "trigMatch_1L2LTrigOR", "trigMatch_2LTrigOR"};
+    vector<string> triggers {"1", "trigMatch_1L2LTrig", "trigMatch_1L2LTrigOR", "trigMatch_2LTrigOR"};
     vector<string> flavors {"lepFlavor[0]==1 && lepFlavor[1]==1", "lepFlavor[0]==2 && lepFlavor[1]==2", "((lepFlavor[0]==2 && lepFlavor[1]==1) || (lepFlavor[0]==1 && lepFlavor[1]==2))"};
-    vector<vector<float>> yields;
 
     //--- make LaTeX table
-    ofstream fout("Table/table.tex", ios::out);
+    ofstream fout("table.tex", ios::out);
     fout << "\\documentclass{article}" << endl;
     fout << "\\usepackage[utf8]{inputenc}" << endl;
     fout << "\\begin{document}" << endl;
@@ -20,24 +19,29 @@ void SUSYTriggers_Table() {
     fout << endl;
 
     for (string trigger : triggers) {
+
+        cout << "Working on table for trigger " << trigger << endl;
+
+        //--- get yields
+        vector<vector<float>> yields;
         for (string file_name : file_names) {
 
             //--- load file
             string ntuple_path = "/eos/atlas/atlascerngroupdisk/phys-susy/2L2J-ANA-SUSY-2018-05/SusySkim2LJets/v1.6/SUSY2/";
             string full_filename = ntuple_path + file_name + "_merged_processed.root";
-            cout << "Working on file " << file_name << endl;
+            cout << "File " << file_name << endl;
             TChain* tch;
             if (file_name.find("data") != string::npos) tch = new TChain("data");
             else tch = new TChain((file_name.substr(file_name.find("/")+1) + "_NoSys").c_str());
             tch->Add(full_filename.c_str());
 
-            //--- get yields
             vector<float> file_yields;
             for (string flavor : flavors) {
-                file_yields.push_back(tch->GetEntries((trigger + " && " + flavor).c_str()));
+                int current_yield = tch->GetEntries((trigger + " && " + flavor).c_str());
+                //cout << current_yield << endl;
+                file_yields.push_back(current_yield);
             }
             yields.push_back(file_yields);
-
             delete tch;
         }
 
@@ -45,7 +49,8 @@ void SUSYTriggers_Table() {
         fout << "\\begin{table}[]" << endl;
         fout << "\\begin{center}" << endl;
         boost::replace_all(trigger, "_", "\\_");
-        fout << trigger << " Counts" << endl;
+        if (trigger == "1") fout << "Inclusive Counts" << endl;
+        else fout << trigger << " Counts" << endl;
         fout << "\\begin{tabular}{l|l|l|l}" << endl;
         fout << "& ee & mm & em \\\\" << endl;
         fout << "\\hline" << endl;
@@ -60,6 +65,6 @@ void SUSYTriggers_Table() {
 
     //--- finish generating LaTeX
     fout << "\\end{document}" << endl;
-    system("latex Table/table.tex");
-    system("dvipdf Table/table.dvi");
+    system("latex table.tex");
+    system("dvipdf table.dvi");
 }
