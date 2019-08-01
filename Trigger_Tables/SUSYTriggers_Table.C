7fbe94da4723c105739a31de2cb225249d853216
Matt Zhang <set.stun@gmail.com>
2019-08-01 02:48:59 +0200

    Added additional trigger to trigger table study

--- Trigger_Tables/SUSYTriggers_Table.C
+++ Trigger_Tables/SUSYTriggers_Table.C
@@ -9,38 +9,37 @@ void SUSYTriggers_Table() {
     vector<string> file_names {"SUSY2_Data/data15-16", "SUSY2_Data/data17", "SUSY2_Data/data18", "SUSY2_Signal_mc16a/C1N2_WZ_600_0_2L2J", "SUSY2_Signal_mc16cd/C1N2_WZ_600_0_2L2J", "SUSY2_Signal_mc16e/C1N2_WZ_600_0_2L2J", "SUSY2_Signal_mc16a/C1N2_WZ_200p0_100p0_2L2J", "SUSY2_Signal_mc16cd/C1N2_WZ_200p0_100p0_2L2J", "SUSY2_Signal_mc16e/C1N2_WZ_200p0_100p0_2L2J"};
     vector<string> file_labels {"data15-16", "data17", "data18", "(600, 0) MC16a", "(600, 0) MC16cd", "(600, 0) MC16e", "(200, 100) MC16a", "(200, 100) MC16cd", "(200, 100) MC16e"};
     string baseline = "(nLep_signal==2 && nJet30>=2 && met_Et>200)";
-    vector<string> triggers {"1", "trigMatch_1L2LTrig", "trigMatch_1L2LTrigOR", "trigMatch_2LTrigOR"};
+    vector<string> triggers {"1", "trigMatch_1L2LTrig", "trigMatch_1L2LTrigOR", "trigMatch_2LTrig", "trigMatch_2LTrigOR"};
     vector<string> flavors {"lepFlavor[0]==1 && lepFlavor[1]==1", "lepFlavor[0]==2 && lepFlavor[1]==2", "((lepFlavor[0]==2 && lepFlavor[1]==1) || (lepFlavor[0]==1 && lepFlavor[1]==2))"};
 
     //--- get yields
-    vector<vector<vector<int>>> yields; // [trigger][file][flavor]
+    vector<vector<vector<int>>> yields; // [file][trigger][flavor]
     vector<vector<vector<int>>> baseline_yields; // additional baseline selection
-    for (string trigger : triggers) {
+    for (string file_name : file_names) {
 
-        cout << "Working on table for trigger " << trigger << endl;
+        //--- load file
+        cout << "Working on trigger table for file " << file_name << endl;
+        string full_filename = ntuple_path + file_name + "_merged_processed.root";
+        TChain* tch;
+        if (file_name.find("data") != string::npos) tch = new TChain("data");
+        else tch = new TChain((file_name.substr(file_name.find("/")+1) + "_NoSys").c_str());
+        tch->Add(full_filename.c_str());
 
-        vector<vector<int>> trigger_yields, trigger_baseline_yields;
-        for (string file_name : file_names) {
+        vector<vector<int>> file_yields, file_baseline_yields;
+        for (string trigger : triggers) {
 
-            //--- load file
-            string full_filename = ntuple_path + file_name + "_merged_processed.root";
-            cout << "File " << file_name << endl;
-            TChain* tch;
-            if (file_name.find("data") != string::npos) tch = new TChain("data");
-            else tch = new TChain((file_name.substr(file_name.find("/")+1) + "_NoSys").c_str());
-            tch->Add(full_filename.c_str());
-
-            vector<int> file_yields, file_baseline_yields;
+            cout << trigger << endl;
+            vector<int> trigger_yields, trigger_baseline_yields;
             for (string flavor : flavors) {
-                file_yields.push_back(tch->GetEntries((trigger + " && " + flavor).c_str()));
-                file_baseline_yields.push_back(tch->GetEntries((baseline + " && " + trigger + " && " + flavor).c_str()));
+                trigger_yields.push_back(tch->GetEntries((trigger + " && " + flavor).c_str()));
+                trigger_baseline_yields.push_back(tch->GetEntries((baseline + " && " + trigger + " && " + flavor).c_str()));
             }
-            trigger_yields.push_back(file_yields);
-            trigger_baseline_yields.push_back(file_baseline_yields);
-            delete tch;
+            file_yields.push_back(trigger_yields);
+            file_baseline_yields.push_back(trigger_baseline_yields);
         }
-        yields.push_back(trigger_yields);
-        baseline_yields.push_back(trigger_baseline_yields);
+        delete tch;
+        yields.push_back(file_yields);
+        baseline_yields.push_back(file_baseline_yields);
     }
 
     //--- make LaTeX table
@@ -65,7 +64,7 @@ void SUSYTriggers_Table() {
         fout << "& ee & mm & em \\\\" << endl;
         fout << "\\hline" << endl;
         for (int i=0; i<file_names.size(); i++)
-            fout << file_labels[i] << " & " << yields[j][i][0] << " & " << yields[j][i][1] << " & " << yields[j][i][2] << " \\\\" << endl;
+            fout << file_labels[i] << " & " << yields[i][j][0] << " & " << yields[i][j][1] << " & " << yields[i][j][2] << " \\\\" << endl;
         fout << "\\end{tabular}" << endl;
         fout << "\\end{center}" << endl;
         fout << "\\end{table}" << endl;
@@ -86,7 +85,7 @@ void SUSYTriggers_Table() {
         fout << "& ee & mm & em \\\\" << endl;
         fout << "\\hline" << endl;
         for (int i=0; i<file_names.size(); i++)
-            fout << file_labels[i] << " & " << (float)baseline_yields[j][i][0]/baseline_yields[0][i][0]*100 << " & " << (float)baseline_yields[j][i][1]/baseline_yields[0][i][1]*100 << " & " << (float)baseline_yields[0][i][2]/baseline_yields[0][i][2]*100 << " \\\\" << endl;
+            fout << file_labels[i] << " & " << (float)baseline_yields[i][j][0]/baseline_yields[i][0][0]*100 << " & " << (float)baseline_yields[i][j][1]/baseline_yields[i][0][1]*100 << " & " << (float)baseline_yields[i][j][2]/baseline_yields[i][0][2]*100 << " \\\\" << endl;
         fout << "\\end{tabular}" << endl;
         fout << "\\end{center}" << endl;
         fout << "\\end{table}" << endl;
